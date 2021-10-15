a5221cca99269e3c0b766c8dd6ff8587ec765dbc
Zhengjiaao <zhengja@dist.com.cn>
2021-10-15 12:58:51 +0800

    feat 新增-flywaydb 模块

---
+++ springboot-test-flywaydb/src/main/java/com/zja/flywaydb/FlywaydbApplication.java
@@ -0,0 +1,13 @@
+package com.zja.flywaydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywaydbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywaydbApplication.class, args);
+    }
+
+}
