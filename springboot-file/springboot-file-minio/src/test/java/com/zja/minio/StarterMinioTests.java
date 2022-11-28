b48aaf97f8df93183461864ba1bafe2e24e8b27b
Zhengjiaao <zhengja@dist.com.cn>
2022-11-28 16:53:24 +0800

    test(minio) 单元测试

---
+++ springboot-file/springboot-file-minio/src/test/java/com/zja/minio/StarterMinioTests.java
@@ -0,0 +1,44 @@
+/**
+ * @Company: 上海数慧系统技术有限公司
+ * @Department: 数据中心
+ * @Author: 郑家骜[ào]
+ * @Email: zhengja@dist.com.cn
+ * @Date: 2022-11-28 16:42
+ * @Since:
+ */
+package com.zja.minio;
+
+import com.zja.FileMinioApplicationTests;
+
+public class StarterMinioTests extends FileMinioApplicationTests {
+//
+//    //客户端操作类
+//    @Resource
+//    MinioClient minioClient;
+//
+//    //桶操作类
+//    @Resource
+//    MinioBucketService minioBucketService;
+//
+//    //对象操作类
+//    @Resource
+//    MinioObjectService minioObjectService;
+//
+//    String bucketName = "testbuket";
+//    String objectName = "3840x2160-12232.jpg";
+//    String filename = "D:\\temp\\images\\3840x2160.jpg";
+//
+//    @Test
+//    public void uploadFile() throws Exception {
+//        /*ObjectWriteResponse response = minioClient.uploadObject(UploadObjectArgs.builder()
+//                .bucket(bucketName)
+//                .object(objectName)  //存储对象id(文件的新名字)
+//                .filename(filename)
+//                .build());
+//        System.out.println(response.toString());*/
+//
+//        //与上面一致
+//        minioObjectService.putObject(objectName, filename);
+//    }
+
+}
