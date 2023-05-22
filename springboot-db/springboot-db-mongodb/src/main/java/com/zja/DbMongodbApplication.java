c6d3202f2711e25fc084a65a025b597b9863d5c0
Zhengjiaao <zhengja@dist.com.cn>
2023-05-22 14:17:24 +0800

    fix 添加注释

--- springboot-db/springboot-db-mongodb/src/main/java/com/zja/DbMongodbApplication.java
+++ springboot-db/springboot-db-mongodb/src/main/java/com/zja/DbMongodbApplication.java
@@ -3,6 +3,9 @@ package com.zja;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * http://localhost:8080/springboot-db-mongodb/swagger-ui.html
+ */
 @SpringBootApplication
 public class DbMongodbApplication {
 
