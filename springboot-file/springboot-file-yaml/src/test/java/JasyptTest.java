5820537192347a3bdc0b64b43e27fbf007f3f81f
Zhengjiaao <zhengja@dist.com.cn>
2023-08-08 14:57:23 +0800

    fix yaml配置内容jasypt加解密

---
+++ springboot-file/springboot-file-yaml/src/test/java/JasyptTest.java
@@ -0,0 +1,52 @@
+/**
+ * @Company: 上海数慧系统技术有限公司
+ * @Department: 数据中心
+ * @Author: 郑家骜[ào]
+ * @Email: zhengja@dist.com.cn
+ * @Date: 2023-08-08 14:32
+ * @Since:
+ */
+
+import com.zja.FileYmlApplication;
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * yaml 配置文件内容加解密
+ *
+ * @author: zhengja
+ * @since: 2023/08/08 14:32
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = FileYmlApplication.class)
+public class JasyptTest {
+
+    @Autowired
+    StringEncryptor encryptor;
+
+    @Value("${jasyptTest}")
+    private String jasyptTest;
+
+    @Test
+    public void test(){
+        //jasypt 自动解密
+        System.out.println(jasyptTest);
+    }
+
+    @Test
+    public void encrypt_and_decrypt(){
+        //加密
+        String hello = encryptor.encrypt("hello");
+        System.out.println(hello);
+
+        //解密
+        String helloTxt = encryptor.decrypt(hello);
+        System.out.println(helloTxt);
+    }
+
+}
