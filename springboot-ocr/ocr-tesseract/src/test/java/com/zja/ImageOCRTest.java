56190cb9fc9a96b00d701d96b1d40d07a2d2c6b6
Zhengjiaao <zhengja@dist.com.cn>
2023-11-06 13:19:36 +0800

    feat 优化代码

--- springboot-ocr/ocr-tesseract/src/test/java/com/zja/ImageOCRTest.java
+++ springboot-ocr/ocr-tesseract/src/test/java/com/zja/ImageOCRTest.java
@@ -15,8 +15,10 @@ import net.sourceforge.tess4j.util.ImageHelper;
 import org.junit.jupiter.api.Test;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -69,6 +71,11 @@ public class ImageOCRTest {
             // 识别
             //String res = instance.doOCR(new File("C:\\Users\\Lenovo\\Pictures\\联想截图\\联想截图_20230220144409.png"));
             String result = instance.doOCR(image);
+
+            // 将识别结果保存到文件
+//            saveResultToFile(result, "output.txt");
+
+            // 输出结果
             System.out.println(result);
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
@@ -117,6 +124,83 @@ public class ImageOCRTest {
             e.printStackTrace();
         }
 
+    }
+
+    /**
+     * 将OCR的文本层叠加在原始图片的上方
+     * 要在OCR图片后在图片上方显示文字层,你可以使用Java的图形处理库（如Java 2D或JavaFX）来实现这个功能。
+     */
+    @Test
+    @Deprecated //todo 未成功，效果不佳
+    public void ocr_image_recognizing_text_4() throws IOException {
+        Tesseract tesseract = new Tesseract();
+        //设置指定训练集位置，推荐配置为全局环境变量：TESSDATA_PREFIX=E:\App\tesseract-ocr\tessdata
+        tesseract.setDatapath("E:\\App\\tesseract-ocr\\tessdata");
+        //设置识别语言(默认是英文识别):指定一个或多个语言，用逗号分隔，例如："eng"表示英语，"chi_sim"表示简体中文。
+        tesseract.setLanguage("chi_sim"); //chi_sim 是中文
+
+        //需要识别的图
+        File imageFile = new File("D:\\temp\\ocr\\input.png");
+
+        String result = null;
+        try {
+            // 读取图像并进行OCR
+            BufferedImage originalImage = ImageIO.read(imageFile);
+            result = tesseract.doOCR(originalImage);
+
+            // 创建带有文本层的新图像
+            BufferedImage imageWithText = createImageWithTextOverlay(originalImage, result);
+
+            // 保存带有文本层的图像
+            ImageIO.write(imageWithText, "jpg", new File("D:\\temp\\ocr\\output.jpg"));
+
+            // 输出识别结果
+            System.out.println(result);
+        } catch (TesseractException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //createImageWithTextOverlay方法创建了一个新的BufferedImage对象，它的宽度和高度与原始图像相同，但添加了一个额外的高度来容纳文本层。
+    //然后，使用Graphics2D对象在新图像上绘制原始图像和文本。
+    //你可以根据需要调整文本的字体、颜色、位置等参数，以及添加更多的绘制操作来自定义文本层。
+    private static BufferedImage createImageWithTextOverlay(BufferedImage originalImage, String text) {
+        // 创建一个新的BufferedImage，包含原始图像的宽度和高度，以及透明的色彩模式
+        BufferedImage imageWithText = new BufferedImage(
+                originalImage.getWidth(), originalImage.getHeight() + 50, BufferedImage.TYPE_INT_ARGB);
+
+        // 获取Graphics2D对象，用于在图像上绘制文本
+        Graphics2D g2d = imageWithText.createGraphics();
+
+        // 将原始图像绘制到新图像中
+        g2d.drawImage(originalImage, 0, 0, null);
+
+        // 设置文本的字体、颜色和位置
+        Font font = new Font("Arial", Font.BOLD, 36);
+        Color color = Color.RED;
+        int x = 10;
+        int y = originalImage.getHeight() + 40;
+
+        // 在新图像上绘制文本
+        g2d.setFont(font);
+        g2d.setColor(color);
+        g2d.drawString(text, x, y);
+
+        // 释放资源
+        g2d.dispose();
+
+        return imageWithText;
+    }
+
+    // 将识别结果保存到文件
+    private static void saveResultToFile(String result, String filePath) {
+        try {
+            FileWriter writer = new FileWriter(filePath);
+            writer.write(result);
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("Failed to save result to file: " + e.getMessage());
+        }
     }
 
 }
