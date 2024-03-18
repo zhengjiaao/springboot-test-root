0933a8036c191b4e4f621ce531e3c0610c6d63be
Zhengjiaao <zhengja@dist.com.cn>
2024-03-18 17:10:25 +0800

    test 单元测试

--- springboot-file/springboot-file-compress/springboot-file-compress-jdk/src/test/java/com/zja/util/JdkZipFileAttributesTest.java
+++ springboot-file/springboot-file-compress/springboot-file-compress-jdk/src/test/java/com/zja/util/JdkZipFileAttributesTest.java
@@ -2,15 +2,23 @@ package com.zja.util;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileAttributeView;
+import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 /**
  * @author: zhengja
@@ -18,9 +26,12 @@ import java.time.format.DateTimeFormatter;
  */
 public class JdkZipFileAttributesTest {
 
+    private final static String zipFilePath = "D:\\temp\\zip\\测试目录.zip";
+
+    // 获取ZIP文件创建时间
     @Test
-    public void test() {
-        Instant zipFileCreationTime = getZipFileCreationTime("D:\\temp\\zip\\测试目录.zip");
+    public void getZipFileCreationTime_test() {
+        Instant zipFileCreationTime = getZipFileCreationTime(zipFilePath);
         System.out.println(formatInstant(zipFileCreationTime, "Asia/Shanghai", "yyyy-MM-dd HH:mm:ss"));
     }
 
@@ -51,6 +62,43 @@ public class JdkZipFileAttributesTest {
         LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, zoneId);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
         return localDateTime.format(formatter);
+    }
+
+
+    // 设置ZIP文件自定义属性、并读取自定义后的属性
+    @Test
+    public void customAttributeToZipFile_test() {
+        setCustomAttributeToZipFile(zipFilePath, "key", "value");
+        String keyValue = getCustomAttributeToZipFile(zipFilePath, "key");
+        System.out.println(keyValue);
+    }
+
+    public void setCustomAttributeToZipFile(String zipFilePath, String attributeName, String attributeValue) {
+        try {
+            Path filePath = Paths.get(zipFilePath);
+            UserDefinedFileAttributeView attributeView = Files.getFileAttributeView(filePath, UserDefinedFileAttributeView.class);
+            // 将属性值转换为字节数组
+            byte[] valueBytes = attributeValue.getBytes(StandardCharsets.UTF_8);
+            // 设置自定义属性
+            attributeView.write(attributeName, ByteBuffer.wrap(valueBytes));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getCustomAttributeToZipFile(String zipFilePath, String attributeName) {
+        try {
+            Path filePath = Paths.get(zipFilePath);
+            UserDefinedFileAttributeView attributeView = Files.getFileAttributeView(filePath, UserDefinedFileAttributeView.class);
+            // 读取自定义属性
+            ByteBuffer buffer = ByteBuffer.allocate(attributeView.size(attributeName));
+            attributeView.read(attributeName, buffer);
+            buffer.flip();
+            return new String(buffer.array());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
