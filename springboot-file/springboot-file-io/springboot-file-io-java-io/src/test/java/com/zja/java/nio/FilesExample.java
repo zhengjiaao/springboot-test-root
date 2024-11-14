9163bfe09792c64b8f9a481f0ae4337d635d82d2
郑家骜[ào] <zhengja@dist.com.cn>
2024-11-14 16:38:31 +0800

    test(io) 单元测试

--- springboot-file/springboot-file-io/springboot-file-io-java-io/src/test/java/com/zja/java/nio/FilesExample.java
+++ springboot-file/springboot-file-io/springboot-file-io-java-io/src/test/java/com/zja/java/nio/FilesExample.java
@@ -6,10 +6,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 
 /**
  * Files 类处理文件,按 Path 对象操作
+ *
  * @author: zhengja
  * @since: 2024/01/22 17:01
  */
@@ -135,4 +137,155 @@ public class FilesExample {
         System.out.println(Files.deleteIfExists(destinationPath));
         System.out.println(Files.deleteIfExists(newLocation));
     }
+
+    @Test
+    public void test_Files_copy() throws IOException {
+        Path sourcePath = Paths.get(filePath);
+        Path destinationPath = Paths.get(filePath_destination);
+
+        // 复制文件，如果目标文件已存在，则覆盖
+        Path copy_path = Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+        System.out.println("复制后的文件路径：" + copy_path);
+    }
+
+    @Test
+    public void test_Files_move() throws IOException {
+        Path sourcePath = Paths.get(filePath);
+        Path destinationPath = Paths.get(filePath_destination);
+
+        // 移动文件，如果目标文件已存在，则覆盖
+        Path move_path = Files.move(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+        System.out.println("移动后的文件路径：" + move_path);
+    }
+
+    @Test
+    public void test_Files_walk() throws IOException {
+        Path rootPath = Paths.get(".");
+        Files.walk(rootPath)
+                .filter(Files::isRegularFile)
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void test_Files_walkFileTree() throws IOException {
+        Path rootPath = Paths.get(".");
+        Files.walkFileTree(rootPath, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                System.out.println("文件：" + file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                System.out.println("目录：" + dir);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                System.out.println("访问文件失败：" + file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                System.out.println("访问目录结束：" + dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+
+    public static void conversionFromChineseToEnglishFiles(Path sourceDir, Path targetDir, String... fileExtensions) throws IOException {
+        if (fileExtensions.length == 0) {
+            fileExtensions = new String[]{".hb"};
+        }
+
+        for (String fileExtension : fileExtensions) {
+            Files.walkFileTree(sourceDir, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (file.toString().endsWith(fileExtension)) {
+                        // 计算目标文件路径
+                        Path targetFile = targetDir.resolve(sourceDir.relativize(file));
+                        // 转为 Java 文件
+                        // toJava(targetFile);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
+    }
+
+    private static void copyHbFiles2(Path sourceDir, Path targetDir) throws IOException {
+        Files.walkFileTree(sourceDir, new SimpleFileVisitor<Path>() {
+            private boolean hasHbFile = false;
+
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                hasHbFile = false; // 重置标志
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (file.toString().endsWith(".hb")) {
+                    hasHbFile = true;
+                    // 计算目标文件路径
+                    Path targetFile = targetDir.resolve(sourceDir.relativize(file));
+                    Path targetSubDir = targetFile.getParent();
+                    if (!Files.exists(targetSubDir)) {
+                        Files.createDirectories(targetSubDir);
+                    }
+                    Files.copy(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
+                    // 转为 Java 文件
+                    // toJava(targetFile);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                if (!hasHbFile) {
+                    // 如果当前目录没有 .hb 文件，删除已创建的空目录
+                    Path targetSubDir = targetDir.resolve(sourceDir.relativize(dir));
+                    if (Files.exists(targetSubDir) && Files.isDirectory(targetSubDir) && Files.list(targetSubDir).count() == 0) {
+                        Files.delete(targetSubDir);
+                    }
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    private static void copyHbFiles3(Path sourceDir, Path targetDir) throws IOException {
+        Files.walkFileTree(sourceDir, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                // 创建目标目录
+                Path targetSubDir = targetDir.resolve(sourceDir.relativize(dir));
+                if (!Files.exists(targetSubDir)) {
+                    Files.createDirectories(targetSubDir);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (file.toString().endsWith(".hb")) {
+                    // 计算目标文件路径
+                    Path targetFile = targetDir.resolve(sourceDir.relativize(file));
+                    Files.copy(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
 }
