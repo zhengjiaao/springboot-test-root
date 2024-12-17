610aedd9ad25dc6c592f14214e054090173b2043
郑家骜[ào] <zhengja@dist.com.cn>
2024-12-17 13:14:47 +0800

    test(poi-tl) 单元测试：图表-x、y轴坐标标题

--- springboot-file/springboot-file-word/springboot-file-word-poi-tl/src/test/java/com/zja/poitl/charts/ChartReferencePolicyTest.java
+++ springboot-file/springboot-file-word/springboot-file-word-poi-tl/src/test/java/com/zja/poitl/charts/ChartReferencePolicyTest.java
@@ -51,6 +51,8 @@ public class ChartReferencePolicyTest {
     private ChartSingleSeriesRenderData createSingleSeriesChart() {
         return Charts.ofSingleSeries("ChartTitle", new String[]{"俄罗斯", "加拿大", "美国", "中国", "巴西", "澳大利亚", "印度"})
                 .series("countries", new Integer[]{17098242, 9984670, 9826675, 9596961, 8514877, 7741220, 3287263})
+                // .setxAsixTitle("X Title") // 可选地，数据类型存在默认轴时，不会生效，只有配置了x轴标题时，才会生效
+                // .setyAsixTitle("Y Title") // 可选地，数据类型存在默认轴时，不会生效，只有配置了y轴标题时，才会生效
                 .create();
     }
 
@@ -70,6 +72,8 @@ public class ChartReferencePolicyTest {
                 .addSeries("countries", new Double[]{15.0, 6.0, 18.0, 231.0, 150.0, 6.0, 118.0, 31.0})
                 .addSeries("speakers", new Double[]{223.0, 119.0, 154.0, 142.0, 223.0, 119.0, 54.0, 42.0})
                 .addSeries("youngs", new Double[]{323.0, 89.0, 54.0, 42.0, 223.0, 119.0, 54.0, 442.0})
+                // .setxAsixTitle("X Title") // todo 未生效，待解决
+                // .setyAsixTitle("Y Title") // 可选地
                 .create();
     }
 
@@ -80,6 +84,8 @@ public class ChartReferencePolicyTest {
                 .addBarSeries("NewBar", new Double[]{223.0, 119.0, 154.0, 142.0})
                 .addLineSeries("youngs", new Double[]{323.0, 89.0, 54.0, 42.0})
                 .addLineSeries("NewLine", new Double[]{123.0, 59.0, 154.0, 42.0})
+                // .setxAsixTitle("X Title")
+                // .setyAsixTitle("Y Title")
                 .create();
     }
 }
