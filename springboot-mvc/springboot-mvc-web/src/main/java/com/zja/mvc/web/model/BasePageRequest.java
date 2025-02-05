bd1a9984c161722db31ce6bbce360e7ec8117ed7
郑家骜[ào] <zhengja@dist.com.cn>
2025-02-05 13:34:10 +0800

    fix(mvc) 优化代码

--- springboot-mvc/springboot-mvc-web/src/main/java/com/zja/mvc/web/model/BasePageRequest.java
+++ springboot-mvc/springboot-mvc-web/src/main/java/com/zja/mvc/web/model/BasePageRequest.java
@@ -2,34 +2,30 @@ package com.zja.mvc.web.model;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
+import javax.validation.constraints.Size;
 
 /**
+ * 分页请求（通用的）
+ *
  * @author: zhengja
- * @since: 2024/03/11 15:21
+ * @since: 2025/02/05 13:24
  */
+@Setter
 @ApiModel("BasePageRequest")
 public class BasePageRequest implements Serializable {
     @ApiModelProperty("页码 从第1页开始")
+    @Size(min = 1, max = 100000000)
     private Integer page = 1;
+    @Getter
     @ApiModelProperty("每页数量 默认 10")
+    @Size(min = 1, max = 100000000)
     private Integer size = 10;
 
     public Integer getPage() {
-        // jpa page 从 0 开始
-        return page - 1;
-    }
-
-    public void setPage(Integer page) {
-        this.page = page;
-    }
-
-    public Integer getSize() {
-        return size;
-    }
-
-    public void setSize(Integer size) {
-        this.size = size;
+        return page - 1; // 注：jpa page 从 0 开始
     }
 }
